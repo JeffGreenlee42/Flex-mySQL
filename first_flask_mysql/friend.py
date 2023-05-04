2cad37e7b9d47f90746b2e813b29c8cdb39fae75
Jeff Greenlee <banjo1@gmail.com>
2023-05-03 17:41:59 -0700

    finished practice assignment MySQL Connection Errors

--- first_flask_mysql/friend.py
+++ first_flask_mysql/friend.py
@@ -14,7 +14,7 @@ class Friend:
     def get_all(cls):
         query = "SELECT * FROM friends;"
         # make sure to call the connectToMySQL function with the schema you are targeting.
-        results = connectToMySQL('Friendships').query_db(query)
+        results = connectToMySQL('first_flask').query_db(query)
         # Create an empty list to append our instances of friends
         friends = []
         # Iterate over the db results and create instances of friends with cls.

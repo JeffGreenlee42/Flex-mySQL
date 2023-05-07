8d924834582ec95930b89d0ca9e81faa8c5781a5
Jeff Greenlee <banjo1@gmail.com>
2023-05-07 14:18:25 -0700

    worked on CRUD lessons... Ignore Linter warnings.. they are false positives

--- Retr_display_data/server.py
+++ Retr_display_data/server.py
@@ -2,22 +2,28 @@ from flask import Flask, render_template, redirect, request
 # import the class from friend.py
 from friend import Friend
 app = Flask(__name__)
+
 @app.route("/")
 def index():
     # call the get all classmethod to get all friends
-    friends = Friend.get_all()
-    print(friends)
-    return render_template("index.html", all_friends = friends)
+    all_friends = Friend.get_all()
+    # print(friends)
+    return render_template("index.html", friends = all_friends)
 
-@app.route('/create_friend', methods=["POST"])
-def create_friend():
-    data = {
-        "first_name": request.form["first_name"],
-        "last_name":  request.form["last_name"],
-        "occupation": request.form["occupation"]
-    }
-    Friend.save(data)
+@app.route('/friends/create', methods=["POST"])
+def create():
+    # data = {
+    #     "first_name": request.form["first_name"],
+    #     "last_name":  request.form["last_name"],
+    #     "occupation": request.form["occupation"]
+    # }
+    Friend.save(request.form)
     return redirect('/')
+
+@app.route('/friend/show/<int:friend_id>')
+def show(friend_id):
+    friend = Friend.get_one(friend_id)
+    return render_template("show_friend.html", friend=friend)
 
 @app.route('/show_friends')
 def show_friends():

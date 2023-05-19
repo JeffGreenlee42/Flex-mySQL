1d25538d5dcba2ffe070a207eb8d4af3afd9b9eb
Jeff Greenlee <banjo1@gmail.com>
2023-05-18 19:25:32 -0700

    Dojos_Ninjas: currently working on getting page Show_Dojo to display ninjas belonging to one Dojo

--- Dojos_Ninjas/flask_app/controllers/controllers_dojo.py
+++ Dojos_Ninjas/flask_app/controllers/controllers_dojo.py
@@ -4,5 +4,14 @@ from flask_app.models.model_dojos import Dojos
 
 @app.route("/")
 def index():
-    return render_template("dojo.html")
+    dojos = Dojos.get_all()
+    return render_template("dojo.html", all_dojos = dojos)
+
+@app.route("/add_new_dojo", methods=["POST"])
+def add_new_dojo():
+    result = Dojos.save(request.form)
+    return redirect("/")
+
+@app.route("/dojos/get_one/<int:dojo_id>")
+def get_one(dojo_id):
     
--- Dojos_Ninjas/flask_app/models/model_dojos.py
+++ Dojos_Ninjas/flask_app/models/model_dojos.py
@@ -1,8 +1,33 @@
 from flask_app.config.mysqlconnection import connectToMySQL
 
-db = "users"
+db = "dojos_and_ninjas_schema"
 
 class Dojos:
 
     def __init__( self,data ):
-        pass
+        self.id = data["id"]
+        self.name = data["name"]
+
+    @classmethod
+    def get_all():
+        query = "SELECT * FROM dojos;"
+        results = connectToMySQL(db).query_db(query)
+        dojos = []
+        for dojo in results:
+            dojos.append(dojo)
+        return dojos
+
+    @classmethod
+    def save(data):
+        query = """INSERT INTO dojos (name)
+                    VALUES (%(name)s )"""
+        results = connectToMySQL(db).query_db(query, data)
+        return results
+    
+    @classmethod
+    def get_one(dojo_id):
+        query = """
+                SELECT * FROM dojos
+                JOIN ninjas
+                ON dojos.id = ninjas.dojo_id;
+                """

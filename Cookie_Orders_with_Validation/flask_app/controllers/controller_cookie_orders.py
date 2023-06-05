7e30c19de5dc2f26ffbcc16c8fd0162fc8cfea2b
JeffGreenlee42 <banjo1@gmail.com>
2023-06-05 03:06:00 -0700

    working on Cookie_orders. Controllers and modesl are next.

--- Cookie_Orders_with_Validation/flask_app/controllers/controller_cookie_orders.py
+++ Cookie_Orders_with_Validation/flask_app/controllers/controller_cookie_orders.py
@@ -6,3 +6,7 @@ from flask_app.models.model_cookie_order import Cookie_orders
 def index():
     # users = Cookie_orders.get_all()
     return render_template("index.html")
+
+@app.route("/cookie_order/new_order")
+def new_order():
+    return render_template("new_order.html")
